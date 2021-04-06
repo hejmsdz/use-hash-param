658f1241d8e7d1d527f3baa4733ae7e48ebdf932
B. Mearns <mearns@users.noreply.github.com>
2021-04-06 10:42:49 -0400

    Support for isomorphic rendering (addresses issue #14) (#15)
    * Fixes #14 - support for Isomorphic rendering
    
    * Update some dependencies to resolve audit issues
    
    * Provide typescript type definitions

--- src/index.js
+++ src/index.js
@@ -3,45 +3,51 @@ const { useState, useEffect, useCallback } = require('react');
 const getHashSearchParams = (location) => {
   const hash = location.hash.slice(1);
   const [prefix, query] = hash.split('?');
-  
+
   return [prefix, new URLSearchParams(query)];
 };
 
-const getHashParam = (key, location = window.location) => {
-  const [_, searchParams] = getHashSearchParams(location);
+const getHashParam = (key, defaultValue) => {
+  if (typeof window === 'undefined') {
+    return defaultValue;
+  }
+  const [_, searchParams] = getHashSearchParams(window.location);
   return searchParams.get(key);
 };
 
-const setHashParam = (key, value, location = window.location) => {
-  const [prefix, searchParams] = getHashSearchParams(location);
+const setHashParam = (key, value) => {
+  if (typeof window !== 'undefined') {
+    const [prefix, searchParams] = getHashSearchParams(window.location);
 
-  if (typeof value === 'undefined' || value === '') {
-    searchParams.delete(key);
-  } else {
-    searchParams.set(key, value);
+    if (typeof value === 'undefined' || value === '') {
+      searchParams.delete(key);
+    } else {
+      searchParams.set(key, value);
+    }
+
+    const search = searchParams.toString();
+    window.location.hash = search ? `${prefix}?${search}` : prefix;
   }
-
-  const search = searchParams.toString();
-  location.hash = search ? `${prefix}?${search}` : prefix;
 };
 
 const useHashParam = (key, defaultValue) => {
-  const [innerValue, setInnerValue] = useState(getHashParam(key));
+  const [innerValue, setInnerValue] = useState(getHashParam(key, defaultValue));
 
   useEffect(() => {
-    const handleHashChange = () => setInnerValue(getHashParam(key));
+    const handleHashChange = () => setInnerValue(getHashParam(key, defaultValue));
+    handleHashChange();
     window.addEventListener('hashchange', handleHashChange);
     return () => window.removeEventListener('hashchange', handleHashChange);
   }, [key]);
-  
+
   const setValue = useCallback((value) => {
-    if (typeof value === 'function') {
-      setHashParam(key, value(getHashParam(key)));
-    } else {
-      setHashParam(key, value);
-    }
-  }, [key]);
-  
+      if (typeof value === 'function') {
+        setHashParam(key, value(getHashParam(key, defaultValue)));
+      } else {
+        setHashParam(key, value);
+      }
+    }, [key]);
+
   return [innerValue || defaultValue, setValue];
 };
 module.exports = useHashParam;
